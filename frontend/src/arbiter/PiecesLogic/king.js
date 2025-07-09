b35a3a848f048d112bf44f7ea5723adeaa2531bf
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-09 21:59:06 +0530

    fixed king logic

--- frontend/src/arbiter/PiecesLogic/king.js
+++ frontend/src/arbiter/PiecesLogic/king.js
@@ -1,37 +1,29 @@
 export const getKingMoves = (from, board, turn) => {
-    const moves = [];
-    const directions = [
+  const moves = [];
+  const directions = [
+    { dr: -1, dc: 0 },
+    { dr: 1, dc: 0 },
+    { dr: 0, dc: -1 },
+    { dr: 0, dc: 1 },
+    { dr: 1, dc: 1 },
+    { dr: 1, dc: -1 },
+    { dr: -1, dc: 1 },
+    { dr: -1, dc: -1 }
+  ];
 
-        { dr: -1, dc: 0 },
-        { dr: 1, dc: 0 },
-        { dr: 0, dc: -1 },
-        { dr: 0, dc: 1 }  ,
-        { dr: 1, dc: 1 },
-        { dr: 1, dc: -1 },
-        { dr: -1, dc: 1 },
-        { dr: -1, dc: -1 }
-    ];
+  for (const { dr, dc } of directions) {
+    const r = from.row + dr;
+    const c = from.col + dc;
 
-    for (const { dr, dc } of directions) {
-        let r = from.row + dr;
-        let c = from.col + dc;
-
-        if(r >= 0 && r < 8 && c >= 0 && c < 8) {
-            const target = board[r][c];
-
-            if (target === '') {
-                moves.push({ row: r, col: c, capture: false });
-            } else {
-                if (target[0] !== turn) {
-                    moves.push({ row: r, col: c, capture: true });
-                }
-                break; // cannot go beyond any piece
-            }
-
-            r += dr;
-            c += dc;
-        }
+    if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+      const target = board[r][c];
+      if (target === '') {
+        moves.push({ row: r, col: c, capture: false });
+      } else if (target[0] !== turn) {
+        moves.push({ row: r, col: c, capture: true });
+      }
     }
+  }
 
-    return moves;
+  return moves;
 };
