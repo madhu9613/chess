a3fd2a4e49cadd96337115af311f3134ad52bbca
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-12 23:00:19 +0530

    changing some structure and adding router to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,49 @@ import React from 'react';
 import Board from './components/Board/Board.jsx';
 import MultiplayerGame from './pages/MultiplayerGame.jsx';
 import Layout from './components/Layout.jsx';
+import LocalPlay from './pages/LocalPlay.jsx';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="min-h-screen grid overflow-hidden bg-black">
-      <Layout showSidebar>
+      <Routes>
+        <Route path="/"
+         element={
+          <Layout showSidebar>
+            <div className='flex justify-center items-center'> 
+                 <Board/>
+            </div>
+             
+          </Layout>
+         
+         }
+        />
+        <Route path='/playlocal'
 
- <MultiplayerGame />
-      </Layout>
+          element={
+            <Layout showSidebar>
 
-     
+              <LocalPlay />
+            </Layout>
+          }
+        />
+
+        <Route path='/playonline'
+        element={
+        <Layout showSidebar>
+          <MultiplayerGame/>
+
+        </Layout>
+        }/>
+
+
+
+
+      </Routes>
+
+
+
     </div>
   );
 };
