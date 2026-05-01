376fd4e022a98bdd2d0aa7da0907cff1a7d2b815
mady9613 <rajkhowamadhujya0@gmail.com>
2026-05-02 03:11:36 +0530

    import thise pieceIcons

---
+++ frontend2/src/utils/pieceIcons.js
@@ -0,0 +1,21 @@
+// Fallback Unicode piece symbols
+export const pieceUnicode = {
+    'wp': '♙', 'wr': '♖', 'wn': '♘', 'wb': '♗', 'wq': '♕', 'wk': '♔',
+    'bp': '♟', 'br': '♜', 'bn': '♞', 'bb': '♝', 'bq': '♛', 'bk': '♚'
+};
+
+// Or use image paths
+export const pieceImages = {
+    'wp': 'assets/pieces/wp.png',
+    'wr': 'assets/pieces/wr.png',
+    'wn': 'assets/pieces/wn.png',
+    'wb': 'assets/pieces/wb.png',
+    'wq': 'assets/pieces/wq.png',
+    'wk': 'assets/pieces/wk.png',
+    'bp': 'assets/pieces/bp.png',
+    'br': 'assets/pieces/br.png',
+    'bn': 'assets/pieces/bn.png',
+    'bb': 'assets/pieces/bb.png',
+    'bq': 'assets/pieces/bq.png',
+    'bk': 'assets/pieces/bk.png',
+};
