adf7b0561a4839fbbfd705e9ecec67922ffa5133
mady9613 <rajkhowamadhujya0@gmail.com>
2026-07-13 02:35:48 +0530

    fixed

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import LocalPlay from './pages/LocalPlay';
 import Practice from './pages/Practice';
+import Analysis from './pages/Analysis';
+import Multiplayer from './pages/Multiplayer';
+import WatchLive from './pages/WatchLive';
+import AuthPage from './pages/AuthPage';
+import AuthCallback from './pages/AuthCallback';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/local" element={<LocalPlay />} />
-          <Route path="/practice" element={<Practice />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/local" element={<LocalPlay />} />
+                <Route path="/practice" element={<Practice />} />
+                <Route path="/analysis" element={<Analysis />} />
+                <Route path="/multiplayer" element={<Multiplayer />} />
+                <Route path="/watch" element={<WatchLive />} />
+              </Routes>
+            </Layout>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
